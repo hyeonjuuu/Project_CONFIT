96bce0ed0762a41bfd571609362c3bce0cb690d7
hyeonjuuu <janghyeonjuu@gmail.com>
2024-01-26 23:25:53 +0900

    [Design] PlayingContents Grid Line 적용 진행중

--- src/components/mainpage/RecentPlayingContents.tsx
+++ src/components/mainpage/RecentPlayingContents.tsx
@@ -1,87 +1,98 @@
 import { useTrendingTVDataStore } from '@/store/useTrendingTVDataStore'
 import styled from 'styled-components'
+import VerticalLinesExample from './VerticalLinesExample'
 
 function RecentPlayingContents() {
   const { trendingTVData } = useTrendingTVDataStore()
   console.log(trendingTVData)
 
   return (
-    <RecentContentsWrapper>
-      {trendingTVData
-        ?.filter(item => item !== undefined)
-        ?.map((item, index) => (
-          <ContentsWrapper key={index}>
-            {index * 4 < trendingTVData.length && (
-              <LargeContentsBox>
-                <ContentsItemBox>
-                  <LargeImg
-                    src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4]?.backdrop_path}`}
-                    alt=""
-                    key={index * 4}
-                    width="720px"
-                  />
-                  <ContentsTextBox>
-                    <ContentsTitle>
-                      {trendingTVData[index * 4]?.name}
-                    </ContentsTitle>
-                    <ContentsDate>
-                      {trendingTVData[index * 4].first_air_date.slice(0, 4)}
-                    </ContentsDate>
-                  </ContentsTextBox>
-                </ContentsItemBox>
-                {index * 4 + 1 < trendingTVData.length && (
+    <>
+      <RecentContentsWrapper>
+        {trendingTVData
+          ?.filter(item => item !== undefined)
+          ?.map((item, index) => (
+            <ContentsWrapper key={index}>
+              {index * 4 < trendingTVData.length && (
+                <LargeContentsBox>
                   <ContentsItemBox>
                     <LargeImg
-                      src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 1]?.backdrop_path}`}
+                      src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4]?.backdrop_path}`}
                       alt=""
-                      key={index * 4 + 1}
+                      key={index * 4}
                       width="720px"
                     />
                     <ContentsTextBox>
                       <ContentsTitle>
-                        {trendingTVData[index * 4 + 1]?.name}
+                        {trendingTVData[index * 4]?.name}
                       </ContentsTitle>
                       <ContentsDate>
-                        {trendingTVData[index * 4 + 1].first_air_date.slice(
-                          0,
-                          4
-                        )}
+                        {trendingTVData[index * 4].first_air_date.slice(0, 4)}
                       </ContentsDate>
                     </ContentsTextBox>
                   </ContentsItemBox>
-                )}
-              </LargeContentsBox>
-            )}
-            {index * 4 + 2 < trendingTVData.length && (
-              <SmallContentsBox>
-                <LargeImg
-                  src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 2]?.poster_path}`}
-                  alt=""
-                  key={index * 4 + 2}
-                  width="480"
-                />
-                {index * 4 + 3 < trendingTVData.length && (
+                  {index * 4 + 1 < trendingTVData.length && (
+                    <ContentsItemBox>
+                      <LargeImg
+                        src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 1]?.backdrop_path}`}
+                        alt=""
+                        key={index * 4 + 1}
+                        width="720px"
+                      />
+                      <ContentsTextBox>
+                        <ContentsTitle>
+                          {trendingTVData[index * 4 + 1]?.name}
+                        </ContentsTitle>
+                        <ContentsDate>
+                          {trendingTVData[index * 4 + 1].first_air_date.slice(
+                            0,
+                            4
+                          )}
+                        </ContentsDate>
+                      </ContentsTextBox>
+                    </ContentsItemBox>
+                  )}
+                </LargeContentsBox>
+              )}
+              {index * 4 + 2 < trendingTVData.length && (
+                <SmallContentsBox>
                   <LargeImg
-                    src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 3]?.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 2]?.poster_path}`}
                     alt=""
-                    key={index * 4 + 3}
+                    key={index * 4 + 2}
                     width="480"
                   />
-                )}
-              </SmallContentsBox>
-            )}
-          </ContentsWrapper>
-        ))}
-    </RecentContentsWrapper>
+                  {index * 4 + 3 < trendingTVData.length && (
+                    <LargeImg
+                      src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 3]?.poster_path}`}
+                      alt=""
+                      key={index * 4 + 3}
+                      width="480"
+                    />
+                  )}
+                </SmallContentsBox>
+              )}
+            </ContentsWrapper>
+          ))}
+      </RecentContentsWrapper>
+      <VerticalLinesExample />
+    </>
   )
 }
 
 export default RecentPlayingContents
 
-const RecentContentsWrapper = styled.div``
+const RecentContentsWrapper = styled.div`
+  /* background-color: white; */
+  /* background-color: transparent; */
+  position: absolute;
+  /* overflow: hidden; */
+`
 
 const ContentsWrapper = styled.div`
   padding: 0 50px;
+  position: relative;
+  z-index: 1;
   margin: 32px 0;
 `
 
