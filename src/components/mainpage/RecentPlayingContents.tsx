8acdb333bccf7bd028a8d273d2e9ecad3ae18fc0
hyeonjuuu <janghyeonjuu@gmail.com>
2024-01-28 23:27:25 +0900

    [Feat] Contents Hover Effect 적용 진행중

--- src/components/mainpage/RecentPlayingContents.tsx
+++ src/components/mainpage/RecentPlayingContents.tsx
@@ -1,5 +1,7 @@
 import { useTrendingTVDataStore } from '@/store/useTrendingTVDataStore'
-import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import styled, { keyframes } from 'styled-components'
+import buttonArrow from '@/assets/buttonArrow.svg'
 
 function RecentPlayingContents() {
   const { trendingTVData } = useTrendingTVDataStore()
@@ -14,13 +16,15 @@ function RecentPlayingContents() {
             <ContentsWrapper key={index}>
               {index * 4 < trendingTVData.length && (
                 <LargeContentsBox>
-                  <ContentsItemBox>
-                    <LargeImg
-                      src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4]?.backdrop_path}`}
-                      alt=""
-                      key={index * 4}
-                      width="720px"
-                    />
+                  <ContentsItemBox to="">
+                    <LargeImgBox>
+                      <LargeImg
+                        src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4]?.backdrop_path}`}
+                        alt=""
+                        key={index * 4}
+                        width="720px"
+                      />
+                    </LargeImgBox>
                     <ContentsTextBox>
                       <ContentsTitle>
                         {trendingTVData[index * 4]?.name}
@@ -31,13 +35,15 @@ function RecentPlayingContents() {
                     </ContentsTextBox>
                   </ContentsItemBox>
                   {index * 4 + 1 < trendingTVData.length && (
-                    <ContentsItemBox>
-                      <LargeImg
-                        src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 1]?.backdrop_path}`}
-                        alt=""
-                        key={index * 4 + 1}
-                        width="720px"
-                      />
+                    <ContentsItemBox to="">
+                      <LargeImgBox>
+                        <LargeImg
+                          src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 1]?.backdrop_path}`}
+                          alt=""
+                          key={index * 4 + 1}
+                          width="720px"
+                        />
+                      </LargeImgBox>
                       <ContentsTextBox>
                         <ContentsTitle>
                           {trendingTVData[index * 4 + 1]?.name}
@@ -53,21 +59,41 @@ function RecentPlayingContents() {
                   )}
                 </LargeContentsBox>
               )}
+
               {index * 4 + 2 < trendingTVData.length && (
                 <SmallContentsBox>
-                  <LargeImg
-                    src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 2]?.poster_path}`}
-                    alt=""
-                    key={index * 4 + 2}
-                    width="480"
-                  />
-                  {index * 4 + 3 < trendingTVData.length && (
+                  <HoverWrapper>
                     <LargeImg
-                      src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 3]?.poster_path}`}
+                      src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 2]?.poster_path}`}
                       alt=""
-                      key={index * 4 + 3}
+                      key={index * 4 + 2}
                       width="480"
                     />
+                  </HoverWrapper>
+                  {index * 4 + 3 < trendingTVData.length && (
+                    <HoverWrapper>
+                      <LargeImg
+                        src={`https://image.tmdb.org/t/p/original/${trendingTVData[index * 4 + 3]?.poster_path}`}
+                        alt=""
+                        key={index * 4 + 3}
+                        width="480"
+                      />
+                      <HoverContents>
+                        <HoverContentsItem>
+                          {trendingTVData[index * 4 + 3]?.name}
+                        </HoverContentsItem>
+                        <HoverContentsDate>
+                          {trendingTVData[index * 4 + 3]?.first_air_date.slice(
+                            0,
+                            4
+                          )}
+                        </HoverContentsDate>
+                        <ViewMoreButton>
+                          View More
+                          <ButtonArrow src={buttonArrow} alt="" />
+                        </ViewMoreButton>
+                      </HoverContents>
+                    </HoverWrapper>
                   )}
                 </SmallContentsBox>
               )}
@@ -104,11 +130,6 @@ const LargeContentsBox = styled.div`
   align-items: center;
 `
 
-const ContentsItemBox = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
 const SmallContentsBox = styled(LargeContentsBox)`
   justify-content: flex-end;
 `
@@ -122,17 +143,133 @@ const LargeImg = styled.img`
 
 const ContentsTextBox = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
+  gap: 0.5rem;
   padding: 16px 10px;
 `
 
 const ContentsTitle = styled.span`
-  font-size: 18px;
+  font-size: 20px;
   font-weight: 600;
 `
 const ContentsDate = styled.span`
   font-family: 'Josefin Sans', sans-serif;
   font-weight: 400;
   color: #707070;
+  font-size: 14px;
+`
+
+const HoverWrapper = styled.a`
+  border-radius: 10px;
+  background: #aaeec4;
+  display: flex;
+  justify-content: center;
+  transition: all 0.5s;
+  overflow: hidden;
+  &:hover {
+    > img {
+      filter: saturate(0%) brightness(70%) blur(2px) opacity(50%);
+      scale: 105%;
+      transition: 0.5s all ease;
+    }
+
+    > div {
+      visibility: visible;
+    }
+  }
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0.5;
+    }
+  }
+`
+
+const ContentsItemBox = styled(Link)`
+  display: flex;
+  flex-direction: column;
+`
+
+const LargeImgBox = styled.div`
+  overflow: hidden;
+  border-radius: 10px;
+  aspect-ratio: 16/9;
+  background: #aaeec4;
+  &:hover {
+    > img {
+      filter: saturate(0%) brightness(70%) blur(2px) opacity(50%);
+      scale: 105%;
+      height: 100%;
+      transition: 0.5s all ease;
+    }
+  }
+`
+
+const LargeHoverWrapper = styled(HoverWrapper)`
+  flex-direction: column;
+  &:hover {
+    > img {
+      filter: saturate(0%) brightness(70%);
+      transition: 0.5s;
+    }
+  }
+`
+
+const HoverContents = styled.div`
+  position: absolute;
+  display: flex;
+  align-self: center;
+  align-items: center;
+  word-break: keep-all;
+  text-align: center;
+  visibility: hidden;
+  flex-direction: column;
+  /* height: 100%; */
+  /* justify-content: space-between; */
+`
+
+const HoverContentsItem = styled.span`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  color: ${({ color }) => (color ? color : '#FFFFFF')};
+  font-size: 24px;
+  font-weight: 600;
+`
+
+const HoverContentsDate = styled(HoverContentsItem)`
+  font-size: 16px;
+  color: white;
+  font-weight: 300;
+  margin: 14px 0;
+`
+const ViewMoreButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #cdcdcd;
+  padding: 10px 20px;
+  border-radius: 30px;
+  /* background-image: url(${buttonArrow});
+  background-position: bottom;
+  background-position-y: 80%;
+  background-position-x: 60%;
+  background-size: 80%;
+  background-repeat: no-repeat; */
+  overflow: hidden;
+  width: 200px;
+  color: #cdcdcd;
+`
+const ButtonArrow = styled.img`
+  position: absolute;
+  width: 120px;
+  padding-top: 6px;
+  margin: 0 auto;
 `
