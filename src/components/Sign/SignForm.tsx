5afa51ba16170925d7001c3cdecaf226dded68e7
hyeonjuuu <janghyeonjuu@gmail.com>
2024-02-04 15:43:06 +0900

    [Feat] 로그인 기능 구현 완료

---
+++ src/components/Sign/SignForm.tsx
@@ -0,0 +1,107 @@
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+
+interface InputProps {
+  marginbottom?: string
+}
+
+interface SignFormProps {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+function SignForm({ onChange }: SignFormProps) {
+  const validateEmailSpan = useRef<HTMLSpanElement>(null)
+  return (
+    <JoinField>
+      <Label htmlFor="email">
+        <JoinText>이메일</JoinText>
+      </Label>
+      <InputField
+        type="email"
+        name="email"
+        placeholder="이메일"
+        id="email"
+        onChange={onChange}
+      />
+      <Validate ref={validateEmailSpan}>이메일이 이미 존재합니다.</Validate>
+    </JoinField>
+  )
+}
+
+export default SignForm
+
+export const JoinWrapper = styled.div`
+  background-color: #edece8;
+  height: 100vh;
+`
+
+export const JoinContainer = styled.div`
+  margin: auto;
+  width: 640px;
+  height: 100vh;
+  background-color: #f0f0ee;
+  display: flex;
+  flex-direction: column;
+`
+
+const LogoBox = styled.div`
+  background-color: red;
+  width: 120px;
+  height: 120px;
+  margin: 0 auto;
+`
+
+export const JoinForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  /* gap: 10px; */
+  margin: auto;
+  /* background-color: yellow; */
+  width: 340px;
+`
+
+const JoinField = styled.p`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 6px 0;
+  /* background-color: teal; */
+`
+
+const Label = styled.label`
+  position: relative;
+  overflow: visible;
+  width: fit-content;
+  height: fit-content;
+`
+
+const InputField = styled.input<InputProps>`
+  /* background-color: red; */
+  height: 36px;
+  border-radius: 6px;
+  border: 1px solid #cdcdcd;
+  padding: 0 8px;
+  margin-bottom: ${({ marginbottom }) => marginbottom};
+`
+
+const SubmitButton = styled.input`
+  height: 48px;
+  border-radius: 6px;
+  background-color: #303032;
+  color: #aaeec4;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+  margin: 4px 0;
+`
+const JoinText = styled.strong`
+  color: #303032;
+`
+const Validate = styled.span`
+  font-size: 12px;
+  padding-left: 6px;
+  visibility: hidden;
+  color: #ec4848;
+  font-weight: 500;
+  margin-bottom: 4px;
+`
---
+++ src/pages/SigninPage.tsx
@@ -0,0 +1,135 @@
+import { useEffect, useRef, useState } from 'react'
+import {
+  InputField,
+  JoinContainer,
+  JoinField,
+  JoinForm,
+  JoinText,
+  JoinWrapper,
+  Label,
+  LogoBox,
+  SubmitButton,
+  Validate
+} from './SignupPage'
+import { supabase } from '@/supabase/supabase'
+import { useNavigate } from 'react-router-dom'
+
+function SigninPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const validateEmailSpan = useRef<HTMLSpanElement>(null)
+
+  const navigate = useNavigate()
+  const goToMain = () => {
+    navigate('/')
+  }
+
+  useEffect(() => {
+    if (email !== '') {
+      setTimeout(() => {
+        validateEmail(email)
+      }, 1500)
+    }
+  }, [email, password])
+
+  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+    // console.log(email)
+  }
+  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  function validateEmail(email: string) {
+    const emailValidation =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    // return emailValidation.test(String(email).toLowerCase())
+    // console.log(emailValidation.test(String(email).toLowerCase()))
+
+    if (emailValidation.test(String(email).toLowerCase()) === true) {
+      if (validateEmailSpan.current !== null) {
+        validateEmailSpan.current.style.visibility = 'hidden'
+      }
+    } else if (emailValidation.test(String(email).toLowerCase()) === false) {
+      if (validateEmailSpan.current !== null) {
+        validateEmailSpan.current.style.visibility = 'visible'
+      }
+    }
+  }
+
+  const signinHandler = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+      })
+      let { data: users, error: userError } = await supabase
+        .from('user')
+        .select('email')
+        .eq('email', email)
+
+      if (data.user && data.session !== null && users) {
+        // alert('로그인 성공')
+        goToMain()
+      } else {
+        alert('이메일과 비밀번호를 확인해주세요')
+      }
+
+      console.log(data)
+    } catch (error) {
+      alert('이메일과 비밀번호를 확인해주세요')
+      console.error(`Error: ${error}`)
+    }
+  }
+
+  return (
+    <JoinWrapper>
+      <JoinContainer>
+        <LogoBox>로고 넣기!</LogoBox>
+        <JoinForm action="">
+          <JoinField>
+            <Label htmlFor="email">
+              <JoinText>이메일</JoinText>
+            </Label>
+            <InputField
+              type="email"
+              name="email"
+              placeholder="이메일을 입력해주세요."
+              id="email"
+              onChange={emailHandler}
+            />
+            <Validate ref={validateEmailSpan}>
+              잘못된 형식의 이메일 주소입니다.
+            </Validate>
+          </JoinField>
+          <JoinField height="92px">
+            <Label htmlFor="password">
+              <JoinText>비밀번호</JoinText>
+            </Label>
+            <InputField
+              name="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요."
+              id="password"
+              onChange={passwordHandler}
+            />
+          </JoinField>
+
+          <JoinField>
+            <SubmitButton
+              type="submit"
+              name="Signin"
+              value="로그인"
+              onClick={e => signinHandler(e)}
+            />
+          </JoinField>
+        </JoinForm>
+      </JoinContainer>
+    </JoinWrapper>
+  )
+}
+
+export default SigninPage
