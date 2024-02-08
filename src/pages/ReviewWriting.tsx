cf28dbfa7f8b24d6805eb62411be6a3d7435d972
hyeonjuuu <janghyeonjuu@gmail.com>
2024-02-08 21:44:04 +0900

    [Feat] SearchData 콘텐츠 포스터 사용, 사용자 이미지 사용 시에 따른 조건식 구현중

--- src/pages/ReviewWriting.tsx
+++ src/pages/ReviewWriting.tsx
@@ -5,18 +5,26 @@ import { debounce } from '@/utils/debounce'
 import { ReactEventHandler, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 
+interface SelectButtonProps {
+  active: boolean
+  onClick: () => void
+}
+
 function ReviewWriting() {
   const [searchKeyword, setSearchKeyword] = useState('')
   const [writingContents, setWritingContents] = useState<SearchDataItems>()
   const { searchResult, setSearchResult } = useSearchStore()
+  const [activeUserImage, setActiveUserImage] = useState('')
   const searchInput = useRef<HTMLInputElement>(null)
   const searchList = useRef<HTMLUListElement>(null)
 
   useEffect(() => {
     const Search = async () => {
       const data = await getSearchData(searchKeyword)
+
       setSearchResult(data.results)
     }
+    setActiveUserImage('영화 포스터')
 
     Search()
   }, [searchKeyword])
@@ -30,29 +38,40 @@ function ReviewWriting() {
     const targetValue = e.currentTarget.value
     if (searchList.current && targetValue) {
       searchList.current.style.visibility = 'visible'
+    } else if (searchList.current !== null) {
+      searchList.current.style.visibility = 'hidden'
     }
     keywordSearch(targetValue)
   }
-  // console.log(searchResult.results)
-  // console.log(searchResult?.map(item => item.poster_path))
 
   const handleSelectContents = (e: React.MouseEvent) => {
     const targetTitle = e.currentTarget.querySelector('span')?.textContent
 
     const searchEqualTitleIndex = searchResult?.find(
-      item => item.title === targetTitle
+      item => (item.title || item.name) === targetTitle
     )
     setWritingContents(searchEqualTitleIndex)
     if (targetTitle && searchInput.current) {
       setSearchKeyword(targetTitle)
       searchInput.current.value = targetTitle
-      // console.log(searchInput.current?.value)
     }
     if (searchList.current) {
       searchList.current.style.visibility = 'hidden'
     }
   }
-  // console.log(writingContents)
+
+  // const handleSelectImage = (e: React.MouseEvent) => {
+  //   const target = e.target
+  //   const targetText = e.currentTarget.textContent
+  //   console.log(target)
+  //   if (targetText) {
+  //     setActiveUserImage(targetText)
+  //   }
+  // }
+  const handleSelectImage = (value: string) => {
+    setActiveUserImage(value)
+  }
+  console.log(searchKeyword)
 
   return (
     <WriteReviewWrapper>
@@ -74,8 +93,8 @@ function ReviewWriting() {
             placeholder="검색어를 입력하세요."
           />
           <SearchList ref={searchList}>
-            {searchResult?.map(item => (
-              <SearchListItems key={item.title}>
+            {searchResult?.map((item, index) => (
+              <SearchListItems key={index}>
                 <SearchListItemSelect
                   type="button"
                   onClick={handleSelectContents}
@@ -84,15 +103,50 @@ function ReviewWriting() {
                     src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
                     alt=""
                   />
-                  <span>{item.title}</span>
+                  <span>{item.title || item.name}</span>
                 </SearchListItemSelect>
               </SearchListItems>
             ))}
           </SearchList>
-          <ButtonContainer>
-            <SelectButton>영화 포스터</SelectButton>
-            <SelectButton>사용자 이미지</SelectButton>
-          </ButtonContainer>
+          <div>
+            <ButtonContainer>
+              <SelectButton
+                type="button"
+                active={activeUserImage === '영화 포스터'}
+                onClick={() => handleSelectImage('영화 포스터')}
+              >
+                영화 포스터
+              </SelectButton>
+              <SelectButton
+                type="button"
+                active={activeUserImage === '사용자 이미지'}
+                onClick={() => handleSelectImage('사용자 이미지')}
+              >
+                사용자 이미지
+              </SelectButton>
+            </ButtonContainer>
+
+            <ImageBox searchkeyword={searchKeyword}>
+              {/* {writingContents === undefined && searchKeyword === '' ? (
+                <EmptyImage>콘텐츠를 검색해보세요.</EmptyImage>
+              ) : (
+                <WritingContentsImage
+                  src={`https://image.tmdb.org/t/p/original/${writingContents?.poster_path}`}
+                  alt={`${writingContents?.title || writingContents?.name} 포스터`}
+                />
+              )} */}
+              {writingContents === undefined && searchKeyword === '' ? (
+                <EmptyImage>콘텐츠를 검색해보세요.</EmptyImage>
+              ) : writingContents && activeUserImage === '영화 포스터' ? (
+                <WritingContentsImage
+                  src={`https://image.tmdb.org/t/p/original/${writingContents?.poster_path}`}
+                  alt={`${writingContents?.title || writingContents?.name} 포스터`}
+                />
+              ) : (
+                <input type="file" />
+              )}
+            </ImageBox>
+          </div>
           <TextAreaP>
             <TextArea name="" id="" cols={30} rows={10}></TextArea>
           </TextAreaP>
@@ -120,7 +174,7 @@ const TitleLine = styled.hr`
 `
 
 const FormContainer = styled.div`
-  margin: 60px auto;
+  margin: 20px auto;
   padding: 12px 0;
   /* background-color: pink; */
   width: 80%;
@@ -136,7 +190,7 @@ const SearchForm = styled.form`
 `
 
 const SearchInput = styled.input`
-  border: 1px solid #c2a97e;
+  border: 1px solid #bab09c;
   height: 46px;
   margin: 0 auto;
   padding: 0 12px;
@@ -153,19 +207,41 @@ const ButtonContainer = styled.div`
   margin: auto;
 `
 
-const SelectButton = styled.button`
-  border: 1px solid #c2a97e;
+const ImageBox = styled.div<{ searchkeyword: string }>`
+  margin-top: 8px;
+  background-color: #fff6ef;
+  width: full;
+  height: 128px;
+  border: 1px solid #bab09c;
+  display: flex;
+  align-items: center;
+`
+
+const EmptyImage = styled.p`
+  margin: auto;
+`
+
+const WritingContentsImage = styled.img`
+  aspect-ratio: 3/4;
+  height: 120px;
+  margin: 4px;
+`
+
+const SelectButton = styled.button<SelectButtonProps>`
+  border: 1px solid #bab09c;
   padding: 8px 16px;
   border-radius: 6px;
   height: 42px;
   width: 49%;
+  background-color: ${({ active }) => (active ? '#303032' : 'transparent')};
+  color: ${({ active }) => (active ? '#aaeec4' : '#bab09c')};
 `
 const TextAreaP = styled.p`
   margin: 0;
 `
 
 const TextArea = styled.textarea`
-  border: 1px solid #c2a97e;
+  border: 1px solid #bab09c;
   outline: none;
   resize: none;
   background-color: #fff6ef;
@@ -188,7 +264,7 @@ const WriteButton = styled.input`
 const SearchList = styled.ul`
   width: 100%;
   height: 320px;
-  background-color: #f5f4f0;
+  background-color: #fffaea;
   position: absolute;
   top: 0;
   margin: 46px 0;
@@ -196,6 +272,7 @@ const SearchList = styled.ul`
   overflow-y: auto;
   transition: 0.5s ease;
   visibility: hidden;
+  border: 1px solid #c2a97e;
 `
 const SearchListItems = styled.li`
   border-bottom: 1px solid #c2a97e;
--- src/types/reviewPage/searchData.ts
+++ src/types/reviewPage/searchData.ts
@@ -3,19 +3,40 @@ interface SearchData {
 }
 
 interface SearchDataItems {
+  name: string | null | undefined
   adult: boolean
   backdrop_path: string
   genre_ids: number[]
   id: number
   media_type: string
   original_language: string
-  original_title: string
+  original_title?: string
   overview: string
   popularity: number
   poster_path: string
   release_date: string
-  title: string
+  title?: string
   video: boolean
   vote_average: number
   vote_count: number
 }
+// interface SearchDataItems {
+//   adult: boolean
+//   backdrop_path: string
+//   genre_ids: number[]
+//   id: number
+//   media_type: string
+//   original_language: string
+//   original_title?: string
+//   overview: string
+//   popularity: number
+//   poster_path: string
+//   release_date: string
+//   title?: string
+//   video: boolean
+//   vote_average: number
+//   vote_count: number
+//   name?: string
+//   origin_country?: string[]
+//   original_name?: string
+// }
